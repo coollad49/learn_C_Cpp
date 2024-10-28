3c1a05a58a25141e34fb5b5e7adb8b30fcb75239
Daniel <daniellucasadebayo@gmail.com>
2024-10-28 19:32:05 +0100

    ..

--- Cpp/Types/enum.cpp
+++ Cpp/Types/enum.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main(){
     enum courses {ALGEBRA, BASIC, PASCAL, CPP, PHILOSOPHY, ANALYSIS, CHEMISTRY, HISTORY};
+    enum grades {A, B, C, D, F} courseGrade; //initialize variable courseGrade.
 
     courses thirdLevel_first_semester = CPP;
 
---
+++ Cpp/Types/typedef.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    typedef int Boolean; // creates an alias, Boolean, for the data type int
+
+    const Boolean TRUE = 1;
+    const Boolean FALSE = 0;
+    Boolean flag;
+}
